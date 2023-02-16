e49a83a2766d15d477033e76879c980138e7c15a
Nga Nguyen <nga260300@gmail.com>
2023-02-16 17:40:07 +0700

    lesson 16, iframe

--- src/test/java/support/ui/SelectEx.java
+++ src/test/java/support/ui/SelectEx.java
@@ -1,13 +1,16 @@
 package support.ui;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 
-public class SelectEx {
+public class SelectEx extends Select {
     private final String OPTION_01= "Option 1";
-    public SlectEx (WebElement element){
+
+    public SelectEx(WebElement element) {
         super(element);
     }
+
     public void selectOption1(){
         selectByVisibleText(OPTION_01);
     }
--- src/test/java/url/Urls.java
+++ src/test/java/url/Urls.java
@@ -3,5 +3,6 @@ package url;
 public interface Urls {
     String baseUrl= "https://the-internet.herokuapp.com";
     String dropdownSlug="/dropdown";
+    String iframeSlug="/iframe";
 
 }
